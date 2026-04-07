aeef2c33dc14f434b63cd91d5c7404554071f5fa
Roy Lu <Roylu0404@gmail.com>
2026-04-07 10:06:04 -0400

    3.2 HW

---
+++ ICD201/unit3/js/ex3.js
@@ -0,0 +1,5 @@
+var last = prompt("Enter your last name:");
+var title = prompt("Enter your title:");
+
+document.write("<h2>By " + last + "</h2>");
+document.write("<b>Hey There</b> <h1>" + title + "</h1>");
